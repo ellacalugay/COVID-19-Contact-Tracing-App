29acb56b525574dee799500a3260ed7af33a3150
Ella <ellacalugay@gmail.com>
2023-07-15 23:42:49 +0800

    Details

---
+++ contact_tracking.py
@@ -0,0 +1,6 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
+
+# Create “your” own covid contact tracing app with GUI
+# Create a program that ask user for typical information found in covid contact tracing app
+# Write the collected information in a file (use any format you like)
+# The app should be able to add and search entry
