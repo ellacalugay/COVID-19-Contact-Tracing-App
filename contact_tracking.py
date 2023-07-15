bf59cb6e27ed61fb771155afc49a97e85d2f876c
Ella <ellacalugay@gmail.com>
2023-07-16 00:13:25 +0800

    Pseudocode

--- contact_tracking.py
+++ contact_tracking.py
@@ -1,6 +1,10 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
 
-# Create “your” own covid contact tracing app with GUI
-# Create a program that ask user for typical information found in covid contact tracing app
-# Write the collected information in a file (use any format you like)
-# The app should be able to add and search entry
+# Pseudocode
+# Ask the user for his/her personal information.
+# Ask the user about COVID-19 Symptoms.
+# Ask the user about Risk Assessment.
+# Ask the user for his/her travel history.
+# Ask the user about contact log.
+# Ask the user about testing and test results for COVID 19.
+# Ash the user if he/she wants to receive notifications from the app.
