609d795942caed89f82deb521d771618ea053e27
Ella <ellacalugay@gmail.com>
2023-07-17 19:14:07 +0800

    Adding background frame file and Pseudocode

---
+++ bg_frame.py
@@ -0,0 +1,9 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
+
+# Pseudocode
+# Import necessary modules
+# Create class for background frame
+# Set the background image
+# Create an exit button
+# Create a turn back button
+# Exit the window. This will exit the program.
