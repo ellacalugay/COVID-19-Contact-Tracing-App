b6f12b5c37c99d8aeb9020245ce66645e9d4ebde
Ella <ellacalugay@gmail.com>
2023-07-17 12:34:32 +0800

    Creating a class and non-parametrized constructor

--- contact_tracking.py
+++ contact_tracking.py
@@ -1,10 +1,23 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
 
 # Pseudocode
+# Create a class named User Information that encapsulates the functionalities for asking the user's personal information and related details.
+class UserInformation:
+    # Create a non-parametrized constructor
+    def __init__(self):
+        self.personal_information = {}
+        self.health_declaration = {}
+        self.risk_assessment = {}
+        self.travel_history = []
+        self.contact_log = []
+        self.covid19_testing = {}
+        self.emergency_information = {}
+        self.receive_notifications = False
 # Ask the user for his/her personal information.
-# Ask the user about COVID-19 Symptoms.
+# Ask the user about Health Declaration.
 # Ask the user about Risk Assessment.
 # Ask the user for his/her travel history.
 # Ask the user about contact log.
 # Ask the user about testing and test results for COVID 19.
+# Ask the user about his/her emergency information.
 # Ash the user if he/she wants to receive notifications from the app.
