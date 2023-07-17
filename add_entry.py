09e874b6c0889f9a6811ece26811fedee43e6ee2
Ella <ellacalugay@gmail.com>
2023-07-17 22:06:43 +0800

    Creating Personal Information Label and getting user's name

--- add_entry.py
+++ add_entry.py
@@ -22,7 +22,22 @@ class AddEntry(tk.Frame):
         canvas.create_image(0, 0, image=self.background_image, anchor="nw")
 
     # Create Personal Information Label
+        self.personal_info_label = tk.Label(self, text="PERSONAL INFORMATION", height=2, font=("Helvetica", 10, "bold"))
+        self.personal_info_label.place(x=15, y=10)
+        self.personal_info_label.config(bg="#B0C4DE")
         # Get user's name
+        self.name = tk.Label(self, text="NAME: ", height=1, font=("Arial", 10, "bold"))
+        self.name.place(x=17, y=50)
+        self.name.config(bg="#FFF68F")
+        
+        # Name Input
+        self.name_entry = tk.Entry(self, width=40)
+        self.name_entry.place(x=70, y= 51)
+        # Set initial text
+        self.name_entry.insert(0, "FIRST NAME/MIDDLE NAME/SURNAME")  
+        self.name_entry.bind("<FocusIn>", self.clear_name_text)
+        self.name_entry.config(fg="gray", bg="#FFF8DC")
+
         # Get user's age
         # Get user's gender
         # Get user's residential address
@@ -50,3 +65,9 @@ class AddEntry(tk.Frame):
         # Ask if the user's received covid vaccine shot.
         # Ask if the user had any symptoms in the past 14 days?
         # Ask if the user have been tested for COVID-19? 
+
+    # Display text will be gone if the user click the entry
+
+    def clear_name_text(self, event):
+        self.name_entry.delete(0, tk.END)
+        self.name_entry.config(fg="black")
