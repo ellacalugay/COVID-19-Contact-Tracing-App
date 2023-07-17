c9a3fb4b5c5bb18ba39406a31c1273919be88438
Ella <ellacalugay@gmail.com>
2023-07-17 18:18:10 +0800

    importing necessary modules and creating class for add entry

--- add_entry.py
+++ add_entry.py
@@ -1,34 +1,42 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
 
 # Pseudocode
+# Import necessar
+import tkinter as tk
+from PIL import ImageTk, Image
+
 # Create a class for add entry
-# Set the background image
-# Create Personal Information Label
-    # Get user's name
-    # Get user's age
-    # Get user's gender
-    # Get user's residential address
+class AddEntry(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
 
-# Create Contact Information label
-    # Get user's contact number
-    # Get user's email address
+    # Set the background image
+    # Create Personal Information Label
+        # Get user's name
+        # Get user's age
+        # Get user's gender
+        # Get user's residential address
 
-# Create Emergency Information label
-    # Get user's name of guardian
-    # Get user's realationship to the guardian
-    # Get user's guardian contact number
-    # Get user's guardian email address
+    # Create Contact Information label
+        # Get user's contact number
+        # Get user's email address
 
-# Create Risk Assessment label
-    # Ask if the user experienced any of the following symptoms: fever, cough, shortness of breath, sore throat, loss of taste or smell, or body aches
-    # Ask if the user have been in close contact with someone who has tested positive for COVID-19.
+    # Create Emergency Information label
+        # Get user's name of guardian
+        # Get user's realationship to the guardian
+        # Get user's guardian contact number
+        # Get user's guardian email address
 
-# Create Travel History label
-    # Ask if the user have traveled to any high-risk areas recently.
-    # Ask if the user have traveled internationally in the past 14 days.
-    # Ask if the user have recently attended large gatherings or events.
+    # Create Risk Assessment label
+        # Ask if the user experienced any of the following symptoms: fever, cough, shortness of breath, sore throat, loss of taste or smell, or body aches
+        # Ask if the user have been in close contact with someone who has tested positive for COVID-19.
 
-# Create Health Declaration label
-    # Ask if the user's received covid vaccine shot.
-    # Ask if the user had any symptoms in the past 14 days?
-    # Ask if the user have been tested for COVID-19? 
+    # Create Travel History label
+        # Ask if the user have traveled to any high-risk areas recently.
+        # Ask if the user have traveled internationally in the past 14 days.
+        # Ask if the user have recently attended large gatherings or events.
+
+    # Create Health Declaration label
+        # Ask if the user's received covid vaccine shot.
+        # Ask if the user had any symptoms in the past 14 days?
+        # Ask if the user have been tested for COVID-19? 
