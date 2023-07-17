bf13fff8a280fa629c2fef85d89535bd8578ac66
Ella <ellacalugay@gmail.com>
2023-07-17 14:39:53 +0800

    Defining the main function and creating instances for user information class

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 
 # Pseudocode
 # Import the UserInformation class from user_information.py
+from contact_tracing import UserInformation
+
 # Define the main function
+def main():
+    # Create an instance of the UserInformation class
+    user_info = UserInformation()
+    
 # Call the methods to ask for the user's information
 # Access the collected user information through the instance's attributes
 # Call the main function
-
