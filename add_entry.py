9e111f7de66f4eff8af5027908a04dc233f06f29
Ella <ellacalugay@gmail.com>
2023-07-18 01:23:20 +0800

    Adding Emergency Info label and getting all the necessary info from the user

--- add_entry.py
+++ add_entry.py
@@ -83,7 +83,7 @@ class AddEntry(tk.Frame):
         self.contact_num = tk.Label(self, text="CONTACT NUMBER: ", height=1, font=("Lucida Bright", 10, "bold"))
         self.contact_num.place(x=17, y=226)
         self.contact_num.config(bg="#EAEAEA")
-        # Residential Address Input
+        # Conatct Number Input
         self.contact_num_entry = tk.Entry(self, width=28)
         self.contact_num_entry.place(x=155, y= 226)
         # Set initial text
@@ -95,7 +95,7 @@ class AddEntry(tk.Frame):
         self.email_add = tk.Label(self, text="EMAIL ADDRESS:  ", height=1, font=("Lucida Bright", 10, "bold"))
         self.email_add.place(x=17, y=251)
         self.email_add.config(bg="#EAEAEA")
-        # Residential Address Input
+        # Email Address Input
         self.email_add_entry = tk.Entry(self, width=30)
         self.email_add_entry.place(x=140, y= 251)
         # Set initial text
@@ -104,10 +104,58 @@ class AddEntry(tk.Frame):
         self.email_add_entry.config(fg="gray", bg="#FFFAFA", font=("Times", 11))
 
     # Create Emergency Information label
+        self.emergency_info_label = tk.Label(self, text=" EMERGENCY INFORMATION ", height=2, font=("Tahoma", 10, "bold"))
+        self.emergency_info_label.place(x=17, y=301)
+        self.emergency_info_label.config(bg="#FFE1FF")
+
         # Get user's name of guardian
+        self.guardian_name = tk.Label(self, text="NAME: ", height=1, font=("Lucida Bright", 10, "bold"))
+        self.name.place(x=17, y=56)
+        self.guardian_name.place(x=17, y=347)
+        self.guardian_name.config(bg="#EAEAEA")
+        # Guardian name Input
+        self.guardian_name_entry = tk.Entry(self, width=40)
+        self.guardian_name_entry.place(x=70, y=347)
+        # Set initial text inside the Entry widget
+        self.guardian_name_entry.insert(0, " FIRST NAME/MIDDLE NAME/SURNAME")
+        self.guardian_name_entry.bind("<FocusIn>", self.clear_guardian_name_text)
+        self.guardian_name_entry.config(fg="gray", bg="#FFFAFA", font=("Times", 11))
+
         # Get user's realationship to the guardian
+        self.guardian_relationship = tk.Label(self, text="RELATIONSHIP:  ", height=1, font=("Lucida Bright", 10, "bold"))
+        self.guardian_relationship.place(x=17, y=372)
+        self.guardian_relationship.config(bg="#EAEAEA")
+        # Guardian Relationship Input
+        self.guardian_relationship_entry = tk.Entry(self, width=33)
+        self.guardian_relationship_entry.place(x=122, y=372)
+        # Set initial text inside the Entry widget
+        self.guardian_relationship_entry.insert(0, " MOTHER/FATHER/SISTER,ETC.")
+        self.guardian_relationship_entry.bind("<FocusIn>", self.clear_guardian_relationship_text)
+        self.guardian_relationship_entry.config(fg="gray", bg="#FFFAFA", font=("Times", 11))
+
         # Get user's guardian contact number
+        self.guardian_contact = tk.Label(self, text="CONTACT NUMBER:  ", height=1, font=("Lucida Bright", 10, "bold"))
+        self.guardian_contact.place(x=17, y=397)
+        self.guardian_contact.config(bg="#EAEAEA")
+        # Guardian Contact Input
+        self.guardian_contact_entry = tk.Entry(self, width=28)
+        self.guardian_contact_entry.place(x=155, y=397)
+        # Set initial text inside the Entry widget
+        self.guardian_contact_entry.insert(0, " 09*********")
+        self.guardian_contact_entry.bind("<FocusIn>", self.clear_guardian_contact_text)
+        self.guardian_contact_entry.config(fg="gray", bg="#FFFAFA", font=("Times", 11))
+
         # Get user's guardian email address
+        self.guardian_email_add = tk.Label(self, text="EMAIL ADDRESS:  ", height=1, font=("Lucida Bright", 10, "bold"))
+        self.guardian_email_add.place(x=17, y=422)
+        self.guardian_email_add.config(bg="#EAEAEA")
+        # Guardian Email Address Input
+        self.guardian_email_add_entry = tk.Entry(self, width=30)
+        self.guardian_email_add_entry.place(x=140, y=422)
+        # Set initial text inside the Entry widget
+        self.guardian_email_add_entry.insert(0, " juandelacruz@gmail.com")
+        self.guardian_email_add_entry.bind("<FocusIn>", self.clear_guardian_email_add_text)
+        self.guardian_email_add_entry.config(fg="gray", bg="#FFFAFA", font=("Times", 11))
 
     # Create Risk Assessment label
         # Ask if the user experienced any of the following symptoms: fever, cough, shortness of breath, sore throat, loss of taste or smell, or body aches
@@ -142,3 +190,15 @@ class AddEntry(tk.Frame):
     def clear_email_add_text(self, event):
         self.email_add_entry.delete(0, tk.END)
         self.email_add_entry.config(fg="black")
+    def clear_guardian_name_text(self, event):
+        self.guardian_name_entry.delete(0, tk.END)
+        self.guardian_name_entry.config(fg="black")
+    def clear_guardian_relationship_text(self, event):
+        self.guardian_relationship_entry.delete(0, tk.END)
+        self.guardian_relationship_entry.config(fg="black")
+    def clear_guardian_contact_text(self, event):
+        self.guardian_contact_entry.delete(0, tk.END)
+        self.guardian_contact_entry.config(fg="black")
+    def clear_guardian_email_add_text(self, event):
+        self.guardian_email_add_entry.delete(0, tk.END)
+        self.guardian_email_add_entry.config(fg="black")
