662d804d22cf11d05effd6fb9aca5aa76f764e21
Ella <ellacalugay@gmail.com>
2023-07-17 14:30:35 +0800

    Adding a new file with pseudocode

---
+++ main.py
@@ -0,0 +1,9 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
+
+# Pseudocode
+# Import the UserInformation class from user_information.py
+# Define the main function
+# Call the methods to ask for the user's information
+# Access the collected user information through the instance's attributes
+# Call the main function
+
