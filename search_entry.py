d628524e49c00fd9530889d6fb38d66e8026d53d
Ella <ellacalugay@gmail.com>
2023-07-23 19:12:21 +0800

    Psuedocode

--- search_entry.py
+++ search_entry.py
@@ -1,1 +1,12 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Final Project
+
+# Pseudocode
+# Create a class named Search Entry
+# Set the image background
+# Create search bar
+# Create search button
+# Create canvass for the results
+# Create canvass for the another results
+# Create exit button
+# Create back button
+# Exit the window. This will exit the program.
